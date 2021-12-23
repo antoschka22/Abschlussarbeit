87184eacfb0d88aba08639295ce6033c773500f4
antoschka22 <antoniomolina2207@gmail.com>
2021-12-23 14:12:03 +0100

    Projekte Endpoints

--- backend/app/source/immosolution/dao/dao_projects.py
+++ backend/app/source/immosolution/dao/dao_projects.py
@@ -1,25 +1,24 @@
 from .db import get_db_cursor
 
-def get_projectnames_dao():
+def get_projects_undone_dao():
     with get_db_cursor() as cursor:
         cursor.execute("""
-                    SELECT * FROM bilder 
-                    WHERE herzeigeprojekte = 'f'
+                    SELECT * FROM projekte
+                        WHERE herzeigeprojekte = 'f'
                        """)
         return cursor.fetchall()
     
 def get_projects_done_dao():
     with get_db_cursor() as cursor:
         cursor.execute("""
-                    SELECT projektbilder, projektname FROM bilder 
-                    join projekte on projekt_id = projekte.id
-                    WHERE herzeigeprojekte = 't'
+                    SELECT * FROM projekte
+                        WHERE herzeigeprojekte = 't'
                        """)
         return cursor.fetchall()
     
 def get_images_by_project_dao(projektid: str):
     with get_db_cursor() as cursor:
         cursor.execute("""
-                    SELECT projektbilder FROM bilder WHERE projektid = %s
+                    SELECT projektbilder FROM bilder WHERE projekt_id = %s
                        """, [projektid])
         return cursor.fetchall()
--- backend/app/source/immosolution/web/controllers/projects.py
+++ backend/app/source/immosolution/web/controllers/projects.py
@@ -2,15 +2,19 @@ from dao.dao_projects import *
 
 def get_projects_undone():
     try:
-        projekt = get_projectnames_dao()
-        projekt['projektbilder'] = get_images_by_project_dao(projekt['id']) 
+        projekt = get_projects_undone_dao()
+        for projektname in projekt:
+            projektname['projektbilder'] = get_images_by_project_dao(projektname['projektname'])
         return projekt, 200
     except:
         return "An error ocurred while fetching the projects", 404
 
 def get_projects_done():
     try:
-        return get_projects_done_dao(), 200
+        projekt = get_projects_done_dao()
+        for projektname in projekt:
+            projektname['projektbilder'] = get_images_by_project_dao(projektname['projektname'])
+        return projekt, 200
     except:
         return "An error ocurred while fetching the projects", 404
     
