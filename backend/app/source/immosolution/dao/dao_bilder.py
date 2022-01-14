58ab6f86462eba02ff05bbc2e6b6ff4d6fd3f9f7
antoschka22 <antoniomolina2207@gmail.com>
2022-01-14 21:28:34 +0100

    backend endpoints

--- backend/app/source/immosolution/dao/dao_bilder.py
+++ backend/app/source/immosolution/dao/dao_bilder.py
@@ -1,9 +1,23 @@
 from .db import get_db_cursor
 
-def dao_remove_bild(bild_id){
+def dao_remove_bild(bild_id):
     with get_db_cursor() as cursor:
         cursor.execute("""
-                    DELETE FROM bilder WHERE id=%s returning id, projektbilder
+                    DELETE FROM bilder WHERE id=%s returning *
                        """, [bild_id])
         return cursor.fetchone()
-}
+
+def dao_add_image(bild):
+    with get_db_cursor() as cursor:
+        cursor.execute("""
+                    INSERT INTO bilder (projektbilder, projekt_id)
+                    VALUES (%s, %s) returning *
+                    """, [bild['projektbilder'], bild['projektname']])
+        return cursor.fetchone()
+        
+def dao_update_image(bild):
+    with get_db_cursor() as cursor:
+        cursor.execute("""
+                    UPDATE bilder SET projektbilder = %s WHERE id=%s returning *
+                    """, [bild['projektbilder'], bild['id']])
+        return cursor.fetchone()
--- backend/app/source/immosolution/web/controllers/bilder.py
+++ backend/app/source/immosolution/web/controllers/bilder.py
@@ -3,12 +3,29 @@ from core.security import *
 
 
 
-def remove_bild(bilder, token_info):
+def remove_bild(project_id, token_info):
     if is_admin(token_info):
-        for bild in bilder:
-            try:
-                dao_remove_bild(bild)
-            except: 
-                return "An error ocurred", 404
+        try:
+            return dao_remove_bild(project_id), 202
+        except:
+            return "An error ocurred", 404
+    else:
+        return "Wrong credentials", 401
+
+def add_image(bild, token_info):
+    if is_admin(token_info):
+        try:
+            return dao_add_image(bild), 201
+        except:
+            return "An error ocurred", 404
+    else:
+        return "Wrong credentials", 401
+
+def update_image(bild, token_info):
+    if is_admin(token_info):
+        try:
+            return dao_update_image(bild), 200
+        except:
+            return "An error ocurred", 404
     else:
         return "Wrong credentials", 401
