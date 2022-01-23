de6c3edfb4ae891b9449a2d3e4427fb0ab8c2cc9
antoschka22 <antoniomolina2207@gmail.com>
2022-01-23 22:47:07 +0100

    Docker environment variables

--- backend/app/source/immosolution/core/security.py
+++ backend/app/source/immosolution/core/security.py
@@ -3,6 +3,3 @@ def is_admin(token_info):
         returns true if the token has the ADMIN role
     """
     return token_info["role"] == 'ADMIN'
-
-# def is_user(token_info):
-#     return token_info["role"] == 'user' or token_info["role"] == 'admin'
